17799a0bcf7946e9be67a36c746d3d46fc04958c
sankalp <sankalp2924@gmail.com>
2024-09-02 15:57:25 +0530

    Delete test1.js

--- test1.js
+++
@@ -1,8 +0,0 @@
-function toggleSidebar() {
-  const sidebar = document.querySelector('.sidebar');
-  if (sidebar.style.left === '-200px') {
-      sidebar.style.left = '0';
-  } else {
-      sidebar.style.left = '-200px';
-  }
-}
